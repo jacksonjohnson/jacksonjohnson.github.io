08288538283958c7f46d60368d5711b5e69443d7
jacksonjohnson <jacksonnoel@Jacksons-MacBook-Air-2.local>
2015-02-07 20:14:43 -0400

    Blog update at 2015-02-08 00:14:43

---
+++ options.js
@@ -0,0 +1,26 @@
+/**
+ * Dbyll Theme Options
+ */
+
+var site = {
+
+  sidebar: {
+    textColor: '#fff',
+    backgroundColor: '#2c3e50',
+  },
+
+  headerColor: '#3ad27a',
+
+  author: {
+    email: "jackson.j.noel@gmail.com",
+    twitter: "jacksonjonson",
+    linkedin: "jacksonnoel",
+  },
+
+  disqus: {
+    shortname: ''
+  },
+
+  analyticsTrackId: ''
+
+}
